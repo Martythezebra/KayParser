42e5a3de2f60c52dc2a449cd9eb38fe1d6a7c6cb
betsygorelik <betsygorelik@gmail.com>
2021-11-29 22:05:05 -0500

    fixed more testing errors

--- CS361 Homework 2/src/TokenStream.java
+++ CS361 Homework 2/src/TokenStream.java
@@ -85,6 +85,7 @@ public class TokenStream {
 
 				if(nextChar == '='){
 					t.setValue(t.getValue() + nextChar);
+					nextChar = readChar();
 					return t;
 				}else{
 					t.setValue(t.getValue());
@@ -97,6 +98,7 @@ public class TokenStream {
 
 				if(nextChar == '='){
 					t.setValue(t.getValue() + nextChar);
+					nextChar = readChar();
 					return t;
 				}else{
 					t.setValue(t.getValue());
@@ -109,6 +111,7 @@ public class TokenStream {
 
 				if(nextChar == '='){
 					t.setValue(t.getValue() + nextChar);
+					nextChar = readChar();
 					return t;
 				}else{
 					t.setValue(t.getValue());
@@ -120,6 +123,7 @@ public class TokenStream {
 				nextChar = readChar();
 				if(nextChar == '='){
 					t.setValue(t.getValue() + nextChar);
+					nextChar = readChar();
 					return t;
 				}else{
 					t.setValue(t.getValue());
@@ -131,6 +135,7 @@ public class TokenStream {
 				nextChar = readChar();
 				if(nextChar == '='){
 					t.setValue(t.getValue() + nextChar);
+					nextChar = readChar();
 					return t;
 				}else{
 					t.setValue(t.getValue());
@@ -188,7 +193,7 @@ public class TokenStream {
 			// now see if this is a keyword
 			if (isKeyword(t.getValue())) {
 				t.setType("Keyword");
-			} else if (t.getValue().equals("true") || t.getValue().equals("false")) {
+			} else if (t.getValue().equals("True") || t.getValue().equals("False")) {
 				t.setType("Literal");
 			}
 			if (isEndOfToken(nextChar)) { // If token is valid, returns.
