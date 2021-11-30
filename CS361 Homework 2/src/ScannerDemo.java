ce880bef2db2225d8e5ea7d0dbfd91d2bea983c0
Martythezebra <martymroger@gmail.com>
2021-11-29 19:10:47 -0500

    Ran initial tests and marked failures with comments

--- CS361 Homework 2/src/ScannerDemo.java
+++ CS361 Homework 2/src/ScannerDemo.java
@@ -6,15 +6,19 @@
 public class ScannerDemo {
 
 	// Oh! This needs to be changed !
-	private static String file1 = "C:\\Users\\cscharff\\eclipse-workspace\\ScannerSolution\\src\\prog1.jay";
+	private static String file1 = "D:\\Git Repositories & Misc\\CS361\\KayParser\\CS361 Homework 2\\src\\prog1.kay";
 	private static int counter = 1;
 
 	public static void main(String args[]) {
 		TokenStream ts = new TokenStream(file1);
 		System.out.println(file1);
+
+		Token tk;
+
 		while (!ts.isEndofFile()) {
 			// TO BE COMPLETED
-			// System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
+			tk = ts.nextToken();
+			System.out.println("Token " + counter++ + " - Type: " + tk.getType() + " - Value: " + tk.getValue());
 		}
 	}
 }
